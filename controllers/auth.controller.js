f47fa326790f00f175d5e5a78953e58aa027ec3d
eduardo moron <eduardomoron151@gmail.com>
2022-05-28 13:44:30 -0400

    Google sign add

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,6 +4,7 @@ const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async(req = request, res = response) => {
 
@@ -50,10 +51,56 @@ const login = async(req = request, res = response) => {
         });
     }
 
+}
+
+const googleSignin = async (req, res = response) => {
+
+    try {
+        const { id_token } = req.body;
+
+        const {correo, nombre, img} = await googleVerify(id_token);
+
+        // Verificar que el correo exista en la base de datos
+        let usuario = await Usuario.findOne({correo});
+
+        if(!usuario) {
+            // tengo que crearlo
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+
+        // si el usuario en BD tiene status en false
+        if(!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // generarl el JWT
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario, 
+            token
+        })
+    } catch (error) {
+        res.status(400).json({
+            msg: 'Token de google no es valido'
+        })
+    }
     
 }
 
 
 module.exports = {
-    login
+    login,
+    googleSignin
 }
